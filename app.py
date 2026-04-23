5c57bab44d4d4440f3412b41aa02bb426cb26101
Riya0899 <riyakohli7355@gmail.com>
2026-04-23 12:32:04 +0530

    -

--- app.py
+++ app.py
@@ -718,7 +718,7 @@ elif page == "📊 Analytics":
 
 # ---------------- HISTORY PAGE ----------------
 # ---------------- HISTORY PAGE ----------------
-elif page == "📜 History":
+elif page == "📜History":
 
     st.markdown("""
     <div style="padding-bottom:20px;">
